866811e5a5e13a1f7f27a007edbbe1dedc82b122
NyoByte <20173527@aloe.ulima.edu.pe>
2020-12-03 20:01:23 -0500

    Eliminar seccion done

--- src/main/java/com/example/demo/controllers/SeccionController.java
+++ src/main/java/com/example/demo/controllers/SeccionController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.dao.AlumnoEntity;
+import com.example.demo.model.dao.ProfesorEntity;
+import com.example.demo.model.dao.SeccionEntity;
 import com.example.demo.model.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class SeccionController {
@@ -26,4 +36,21 @@ public class SeccionController {
     private SeccionRepostitory seccionRep;
     @Autowired
     private UsuarioRepository usuarioRep;
+
+    @RequestMapping(value = "/seccion/eliminar/{id}", method = RequestMethod.POST)
+    public String eliminarSeccion(@PathVariable String id){
+        Optional<SeccionEntity> seccionSeleccionado = seccionRep.findById(Long.parseLong(id));
+        if(seccionSeleccionado.isPresent()){
+            //Eliminar a los alumnos de esa seccion
+            seccionSeleccionado.get().setAlumnos(null);
+            seccionRep.save(seccionSeleccionado.get());
+            //Eliminar a los profesores de esa seccion
+            seccionSeleccionado.get().setProfesor(null);
+            seccionRep.save(seccionSeleccionado.get());
+            //Eliminar la seccion
+            seccionRep.delete(seccionSeleccionado.get());
+        }
+        return  "redirect:/seccion";
+    }
+
 }
